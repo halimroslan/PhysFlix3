8bc0326cf0d0b410525c39c83444ba50e8581453
halimroslan <abdulhalimroslan@gmail.com>
2026-08-11 12:03:42 +0800

    feat: Add Tavis blocker for 4.3a Haba Pendam Tentu at grid F1-G2 (portrait non-fullscreen), D1-E2 (portrait fullscreen) and D1-E3 (landscape fullscreen) for mobile views

--- src/components/VideoPlayerView.tsx
+++ src/components/VideoPlayerView.tsx
@@ -738,15 +738,26 @@ export const VideoPlayerView: React.FC<VideoPlayerViewProps> = ({
 
             {/* Conditional Tavis Censor Block for 4.3a Haba Pendam Tentu (PC View D1-E2) */}
             {currentLesson.titleBm === "4.3a Haba Pendam Tentu" && (
-              <div 
-                className="absolute z-20 pointer-events-auto cursor-default bg-[#0a0a0a] hidden md:block"
-                style={{
-                  left: '0%',         // Col 1 start
-                  top: '11.538%',     // Row D start
-                  width: '13.333%',   // Col 1 to 2 (2 cols)
-                  height: '7.692%'    // Span 2 rows (D to E)
-                }}
-              ></div>
+              <>
+                <div 
+                  className="absolute z-20 pointer-events-auto cursor-default bg-[#0a0a0a] hidden md:block"
+                  style={{
+                    left: '0%',         // Col 1 start
+                    top: '11.538%',     // Row D start
+                    width: '13.333%',   // Col 1 to 2 (2 cols)
+                    height: '7.692%'    // Span 2 rows (D to E)
+                  }}
+                ></div>
+                
+                {/* Mobile View Blockers */}
+                <div 
+                  className={`absolute left-0 z-20 pointer-events-auto cursor-default bg-[#0a0a0a] md:hidden h-[7.692%] ${
+                    isFullscreen
+                      ? "portrait:top-[11.538%] portrait:w-[13.333%] landscape:top-[11.538%] landscape:w-[20%]"
+                      : "top-[19.23%] w-[13.333%]"
+                  }`}
+                ></div>
+              </>
             )}
 
             {/* Conditional PHYSFLIX Cover for 5.6b Interferens Gelombang */}
